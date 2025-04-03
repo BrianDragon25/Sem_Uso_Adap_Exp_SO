705b882ce66409e20265889607d99ada85008820
BrianDragon25 <168883900+BrianDragon25@users.noreply.github.com>
2025-04-02 18:45:33 -0600

    Add files via upload

---
+++ Hands-on 2/Batch Script 1/helloworld.bat
@@ -0,0 +1,15 @@
+@echo off
+rem Imprimir "Hello World"
+echo Hello World
+
+rem Listar contenido del directorio actual
+dir
+
+rem Crear un subdirectorio llamado Test
+mkdir Test
+
+rem Cambiarse al subdirectorio Test
+cd Test
+
+rem Listar contenido del subdirectorio Test
+dir
