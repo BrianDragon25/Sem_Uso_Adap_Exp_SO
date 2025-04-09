556e713b1b783a08ebd140a09f23cae53b62e9b2
BrianDragon25 <168883900+BrianDragon25@users.noreply.github.com>
2025-04-08 20:56:36 -0600

    Add files via upload

---
+++ Hands-on 3/Linux Script 1/helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Imprimir en pantalla “Hello World”
+echo "Hello World"
+
+# Listar el contenido del directorio actual
+echo "Contenido del directorio actual:"
+ls
+
+# Crear un directorio llamado Test
+mkdir -p Test
+
+# Cambiarse al directorio Test
+cd Test
+
+# Listar el contenido del directorio Test
+echo "Contenido del directorio Test:"
+ls
+
+
